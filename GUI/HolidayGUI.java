a35e0fcb8ec1f3ae9450df99c5baad9fd1ed795c
Alexander Stephen Davies <daviesa1@gfpc035.cs.man.ac.uk>
2013-03-13 16:03:29 +0000

    Recommented some code after the merge messed up

--- GUI/HolidayGUI.java
+++ GUI/HolidayGUI.java
@@ -29,54 +29,59 @@ public class HolidayGUI extends Window implements ActionListener
     this.driver = driver;
     this.requests = requests;
     lblWelcome.setText("Hello " + driver.getName());
-	  holidays_left.setText("You have " + driver.getHolidaysLeft() + " days left");
-	  if(requests == null)
-	  {
-		  hasHolidays = false;
-	  }
-	  else
-	  {
-		  hasHolidays = true;
-		  this.start_date = new JLabel[requests.length];
-		  this.end_date = new JLabel[requests.length];	  
-		  lblWelcome.setText("Hello " + driver.getName());
-		  holidays_left.setText(driver.getHolidaysLeft() + " days left");
-		  for(int i = 0; i < requests.length; i++)
-		  {
-			start_date[i] = new JLabel(DateFormat.getDateInstance().format(requests[i].getStartDate()));
-			end_date[i] = new JLabel(DateFormat.getDateInstance().format(requests[i].getEndDate()));		
-		  }
-	  }
+    holidays_left.setText("You have " + driver.getHolidaysLeft() + " days left");
+    //Check if null to fix crash if no Holidays
+    if(requests == null)
+      hasHolidays = false;
+    else
+    {
+      hasHolidays = true;
+      this.start_date = new JLabel[requests.length];
+      this.end_date = new JLabel[requests.length];	  
+      lblWelcome.setText("Hello " + driver.getName());
+      holidays_left.setText(driver.getHolidaysLeft() + " days left");
+      //Loop through all requests and each pair to the window
+      for(int i = 0; i < requests.length; i++)
+      {
+        start_date[i] = new JLabel(DateFormat.getDateInstance().format(requests[i].getStartDate()));
+	end_date[i] = new JLabel(DateFormat.getDateInstance().format(requests[i].getEndDate()));		
+      }
+    }
   }
  
-    public void show(MainGUI _window)
+  /**
+   * Shows the GUI window and adds the labels and buttons
+   */
+  public void show(MainGUI _window)
   {
     window = _window;
     contents = window.getContentPane();
-	if(hasHolidays)
-	{
-	    contents.setLayout(new GridLayout(requests.length + 3, 2));
-	    contents.add(lblWelcome);
-	    contents.add(holidays_left);
-	    contents.add( new JLabel("Start Dates"));
-	    contents.add(new JLabel("End Dates"));
-	    for(int i=0; i< requests.length; i++)
-	    {
-	      contents.add(start_date[i]);
-	      contents.add(end_date[i]);
-	    }
-	}
-	else
-	{
-		contents.setLayout(new GridLayout(3, 2));
-	    contents.add(lblWelcome);
-	    contents.add(holidays_left);
-	    contents.add(new JLabel("You have no holidays booked"));
-	    contents.add(new JLabel(""));
-	}
-	contents.add(btnReturn);
+    //Two windows
+    //One for a driver with Holidays
+    if(hasHolidays)
+    {
+      contents.setLayout(new GridLayout(requests.length + 3, 2));
+      contents.add(lblWelcome);
+      contents.add(holidays_left);
+      contents.add( new JLabel("Start Dates"));
+      contents.add(new JLabel("End Dates"));
+      for(int i=0; i< requests.length; i++)
+      {
+        contents.add(start_date[i]);
+        contents.add(end_date[i]);
+      }
+    }
+    //And another for a driver with no holidays
+    else
+    {
+      contents.setLayout(new GridLayout(3, 2));
+      contents.add(lblWelcome);
+      contents.add(holidays_left);
+      contents.add(new JLabel("You have no holidays booked"));
+      contents.add(new JLabel(""));
+    }
+    contents.add(btnReturn);
     btnReturn.addActionListener(this);
-    
   }
  
   /**
